5a535e9d4c09b044ceee53d720f4101ccaea84c6
wagneraos <wagner.alves@asapcred.com.br>
2023-08-06 10:28:01 -0300

    add: challenge string - doublechars

---
+++ challenges/strings/double-chars/main.go
@@ -0,0 +1,33 @@
+package main
+
+/*
+In this challenge, you are tasked with implementing a function DoubleChars
+which will take in a string and then return another string which has every letter in the word doubled.
+
+https://tutorialedge.net/challenges/go/repeating-letters/
+*/
+
+import (
+	"fmt"
+	"strings"
+)
+
+func DoubleChars(original string) string {
+	//Initially, we declare an array with double the size of the original string that is being passed as the function parameter.
+	result := make([]string, len(original)*2)
+
+	//I gonna traverse the original string to obtain every character of the string.
+	for _, v := range original {
+		//I gonna append every character in a double way til the end of the original string.
+		result = append(result, string(v), string(v))
+	}
+	return strings.Join(result, "")
+}
+
+func main() {
+	fmt.Println("Smallest Difference Challenge")
+
+	original := "gophers"
+	doubled := DoubleChars(original)
+	fmt.Println(doubled) // ggoopphheerrss
+}
