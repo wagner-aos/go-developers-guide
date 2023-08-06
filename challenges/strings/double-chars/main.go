b7620f5150ec277954b581fb48baaed9c8b54eea
wagneraos <wagner.alves@asapcred.com.br>
2023-08-06 10:33:22 -0300

    update: challenge string - doublechars

--- challenges/strings/double-chars/main.go
+++ challenges/strings/double-chars/main.go
@@ -9,20 +9,28 @@ https://tutorialedge.net/challenges/go/repeating-letters/
 
 import (
 	"fmt"
-	"strings"
 )
 
 func DoubleChars(original string) string {
-	//Initially, we declare an array with double the size of the original string that is being passed as the function parameter.
-	result := make([]string, len(original)*2)
-
-	//I gonna traverse the original string to obtain every character of the string.
-	for _, v := range original {
-		//I gonna append every character in a double way til the end of the original string.
-		result = append(result, string(v), string(v))
+	endString := make([]rune, 0, len(original)*2)
+	for _, c := range original {
+		endString = append(endString, c, c)
 	}
-	return strings.Join(result, "")
+	return string(endString)
 }
+
+////Alternative function
+// func DoubleChars(original string) string {
+// 	//Initially, we declare an array with double the size of the original string that is being passed as the function parameter.
+// 	result := make([]string, len(original)*2)
+
+// 	//I gonna traverse the original string to obtain every character of the string.
+// 	for _, v := range original {
+// 		//I gonna append every character in a double way til the end of the original string.
+// 		result = append(result, string(v), string(v))
+// 	}
+// 	return strings.Join(result, "")
+// }
 
 func main() {
 	fmt.Println("Smallest Difference Challenge")
