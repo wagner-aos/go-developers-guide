9336a08acd978f28d5f0de8dcdf17b3056b65a75
wagneraos <wagner.alves@asapcred.com.br>
2023-08-06 19:26:00 -0300

    update: challenges/strings - punctuation substitution

--- challenges/strings/punctuation-substitution/main.go
+++ challenges/strings/punctuation-substitution/main.go
@@ -21,14 +21,14 @@ func changeCharacters(text string) string {
 
 	isExclamation := false
 	for i := 0; i < len(slice); i++ {
-		if slice[i] == "." { // every ocurrency of "." is gonna be changed to "!"
+		if slice[i] == "." { // every ocurrence of "." is gonna be changed to "!"
 			result = append(result, "!")
 		} else if slice[i] == "!" && !isExclamation { //isExclamation controls if the last character added was a "!"
 			result = append(result, slice[i], "!") //Append an aditional "!" to the 'result' slice
-			isExclamation = true                   //change the last character added to "!", so it means if the next character is a "!", it won't be added!
+			isExclamation = true                   //change the last character added status to "!", so it means if the next character is a "!", it won't be added!
 		} else {
-			result = append(result, slice[i]) //Adds all character
-			//if the current character is not a "!", it sets isExclamation to false,
+			result = append(result, slice[i]) //It adds all character
+			//if the current character is not a "!", it sets the isExclamation to false,
 			//thus, if the next character is a "!", then it will be added to 'result' slice
 			if slice[i] != "!" {
 				isExclamation = false
